4b89cafc52b9a79bd4795f82bc7ea0f21e44853a
Roberto-Parra <w.parrav@uniandes.edu.co>
2021-06-20 01:08:44 -0400

    Se agrega tabla Show

--- app.py
+++ app.py
@@ -39,6 +39,11 @@ class Venue(db.Model):
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
+    website = db.Column(db.String(500))
+    seeking_talent = db.Column(db.Boolean(), default=False)
+    seeking_description = db.Column(db.String(500))
+    shows = db.relationship('Show', backref='venue', lazy=True)
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
@@ -53,10 +58,24 @@ class Artist(db.Model):
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    website = db.Column(db.String(500))
+    seeking_venue = db.Column(db.Boolean(), default=False)
+    seeking_description = db.Column(db.String(500))
+    shows = db.relationship('Show', backref='artist', lazy=True)
+
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+class Show(db.Model):
+    __tablename__ = 'Show'
+
+    id = db.Column(db.Integer, primary_key=True)
+    artist_id = db.Column(db.Integer,db.ForeignKey('Artist.id'),nullable=False)
+    venue_id = db.Column(db.Integer,db.ForeignKey('Venue.id'),nullable=False)
+    start_time = db.Column(db.DateTime(), nullable=False)
+
+
 
 #----------------------------------------------------------------------------#
 # Filters.
